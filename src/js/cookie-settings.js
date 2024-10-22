edada5331afcdd5083be7ebed35f762be161210e
mykhaito <163899158+mykhaito@users.noreply.github.com>
2024-10-22 20:05:50 +0200

    wef

--- src/js/cookie-settings.js
+++ src/js/cookie-settings.js
@@ -1,4 +1,4 @@
-(() => {
+document.addEventListener('DOMContentLoaded', () => {
   const refs = {
     // Додати атрибут data-cookie-open на кнопку відкриття
     openModalBtn: document.querySelector('[data-cookie-open]'),
@@ -15,4 +15,4 @@
     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
     refs.modal.classList.toggle('is-open');
   }
-})();
+});
